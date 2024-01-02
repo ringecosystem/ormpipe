dedfc04579ebe528ea5b6a67ab5ae0c2c56f9d36
fewensa <osuni@protonmail.com>
2024-01-02 08:27:57 +0000

    add apinode

--- packages/relay-apinode/src/ormp_relay.ts
+++ packages/relay-apinode/src/ormp_relay.ts
@@ -1,5 +1,15 @@
+import {OrmpRelayConfig} from "@darwinia/ormpipe-relay-airnode";
 
 
 export class OrmpRelayApinode {
+
+  constructor(
+    private readonly config: OrmpRelayConfig,
+  ) {
+  }
+
+  public async start() {
+
+  }
 
 }
