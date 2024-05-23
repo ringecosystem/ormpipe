b240c2426751b062ab9b94e303e3a1b7d44d6016
Guantong <i@gt.email>
2024-05-23 15:38:00 +0800

    Update gasLimit

--- packages/relay-relayer/src/relay.ts
+++ packages/relay-relayer/src/relay.ts
@@ -176,7 +176,7 @@ export class RelayerRelay extends CommonRelay<RelayerRelayLifecycle> {
     try {
       targetTxRelayMessage = await this.targetRelayerClient.relay({
         message,
-        gasLimit: BigInt(sourceNextMessageAccepted.messageGasLimit) + baseGas,
+        gasLimit: BigInt(sourceNextMessageAccepted.messageGasLimit * 63 / 64) + baseGas + 100000,
         chainId: options.targetChainId,
       });
     } catch (e: any) {
